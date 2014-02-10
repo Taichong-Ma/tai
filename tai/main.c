fd3c89a18e2c376d0713849d31566b4118e8d6e1
Taichong Ma <taichong@Taichongs-MacBook-Air.local>
2014-02-10 12:42:05 -0500

    Initial Commit

---
+++ tai/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tai
+//
+//  Created by Taichong Ma on 2/10/14.
+//  Copyright (c) 2014 Taichong Ma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
